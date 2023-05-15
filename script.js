22ae7564f3ddc7ec883d5d7ede9103a11b275413
Francesc Bagué Martí <fbaguem2021@cepnet.net>
2023-05-15 13:29:22 +0200

    Inicio del portafolios

---
+++ script.js
@@ -0,0 +1,29 @@
+// window.addEventListener('load',()=>{
+    // const btn = document.querySelector('#btn')
+    // btn.addEventListener('click',function() { alert('este es el onclick') })
+// })
+// const main = ()=>{
+//     const btn = document.querySelector('#btn')
+//     btn.addEventListener('click',function() { alert('este es el onclick') })
+// }
+function toggleTheme() {
+    const body = document.querySelector('body')
+    if (body.dataset.bsTheme == 'light') {
+        body.dataset.bsTheme = 'dark'
+    } else if (body.dataset.bsTheme == 'dark') {
+        body.dataset.bsTheme = 'light'
+    }
+}
+const navTabs = document.querySelectorAll('.nav-tabs > * > .nav-link')
+navTabs.forEach(e => {
+    e.addEventListener('click', (event) => {
+        document.querySelector('.active').classList.remove('active')
+        document.querySelector('.card:not(.hide)').classList.add('hide')
+        const btn = event.target
+        btn.classList.add('active')
+        const target = document.querySelector(btn.dataset.target)
+        target.classList.remove('hide')
+        // console.log(self.target.dataset.target)
+    })
+})
+// window.addEventListener("load",main);
