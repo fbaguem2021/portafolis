5a9a8c431d7f3047534afcbc8f0c29bdb3aa499a
Francesc Bagué Martí <fbaguem2021@cepnet.net>
2023-05-23 13:55:50 +0200

    avances

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,26 +5,50 @@ const navbar = Vue.createApp({
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+            <button style="color: yellow;" class="btn btn-transparent nav-logo"><i class="bi bi-sun-fill"></i></button>
             <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-                <ul class="navbar-nav mb-2 mb-lg-0 nav-links d-flex flex-row-reverse">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#">Home</a>
+                <ul class="navbar-nav navbar-animated mb-2 mb-lg-0 nav-links">
+                    <li class="nav-item" style="height: 46px;">
+                        <img class="nav-icon" src="./fb-logo2-white.svg" alt="" width="32" height="32">
                     </li>
-                    <li class="nav-item"><a href="" class="nav-link">Test</a></li>
-                    <li class="nav-item">
-                        <a type="button" class="nav-link" data-bs-target="#carouselExample" data-bs-slide-to="2" aria-label="Slide 2">Last</a>
-                    </li>
-                    <li class="nav-item">
-                        <a type="button" class="nav-link" data-bs-target="#carouselExample" data-bs-slide-to="1" aria-label="Slide 1">Second</a>
-                    </li>
-                    <li class="nav-item">
-                        <a type="button" class="nav-link" data-bs-target="#carouselExample" data-bs-slide-to="0" aria-label="Slide 0">First</a>
-                    </li>
+                    <div class="nav-links-container">
+                        <li v-for="link in links" :key="link.lbl"
+                            class="nav-item">
+                            <a type="button" class="nav-link" data-bs-target="#carouselExample" :data-bs-slide-to="link.target" :aria-label="link.lbl">
+                                <i class="bi" :class="link.icon"></i> {{ link.txt }}
+                            </a>
+                        </li>
+                    </div>
                 </ul>
-                <button style="color: yellow;" class="btn btn-outline-primary nav-icon"><i class="bi bi-sun-fill"></i></button>
-                <span class="nav-space"></span>
             </div>
         </div>
     </nav>
-    `
+    `,
+    data() {
+        return {
+            links_count: Number
+        }
+    },
+    methods: {
+        carousel(txt, icon) {
+            if (this.links_count === Number) {
+                this.links_count = 0; 
+            } else {
+                this.links_count++
+            }
+            const target = this.links_count
+            return { carousel: true, id: '#carouselExample', target, txt, lbl:`Slide ${target}`, icon: `bi-${icon}` }
+        }
+    },
+    computed: {
+        links() {
+            return [
+                this.carousel('About Me', 'person-vcard'),
+                this.carousel('Projects', 'briefcase'),
+                this.carousel('Languages', 'globe-americas'),
+                this.carousel('Hard/Soft Skils', 'lightning'),
+                this.carousel('Contact', 'telephone'),
+            ]
+        }
+    }
 })
